f4b85f24bd14ab63cfeeb7913af7f2acde3b7049
Anand Saini <125125041+anand44xy@users.noreply.github.com>
2024-12-21 18:44:09 +0530

    Email-verification & Login Controller added

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,7 +4,7 @@ import { registerUserController, verifyEmailController } from "../Controllers/us
 const userRouter = Router()
 
 userRouter.post('/register', registerUserController)
-userRouter.post('verify_email', verifyEmailController )
+userRouter.post('/verify_email', verifyEmailController )
 
 export default userRouter
 
